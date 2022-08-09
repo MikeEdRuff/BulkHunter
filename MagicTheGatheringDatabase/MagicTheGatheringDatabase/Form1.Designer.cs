f7f44441e4a96a08c03cbf99b9833ecaa350b7f4
MichaelRuff <43830631+mikeeddog@users.noreply.github.com>
2022-08-09 00:31:58 -0400

    Add files via upload

---
+++ MagicTheGatheringDatabase/MagicTheGatheringDatabase/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace MagicTheGatheringDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ownedCardsListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.removeAllButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ownedCardsListBox
+            // 
+            this.ownedCardsListBox.FormattingEnabled = true;
+            this.ownedCardsListBox.Location = new System.Drawing.Point(38, 62);
+            this.ownedCardsListBox.Name = "ownedCardsListBox";
+            this.ownedCardsListBox.Size = new System.Drawing.Size(725, 342);
+            this.ownedCardsListBox.TabIndex = 0;
+            this.ownedCardsListBox.SelectedIndexChanged += new System.EventHandler(this.ownedCardsListBox_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(598, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(526, 415);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // removeAllButton
+            // 
+            this.removeAllButton.Location = new System.Drawing.Point(607, 415);
+            this.removeAllButton.Name = "removeAllButton";
+            this.removeAllButton.Size = new System.Drawing.Size(75, 23);
+            this.removeAllButton.TabIndex = 4;
+            this.removeAllButton.Text = "Remove All";
+            this.removeAllButton.UseVisualStyleBackColor = true;
+            this.removeAllButton.Click += new System.EventHandler(this.removeAllButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(688, 415);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "1";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Card Name";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.removeAllButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ownedCardsListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ownedCardsListBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button removeAllButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MagicTheGatheringDatabase/MagicTheGatheringDatabase/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace MagicTheGatheringDatabase
+{
+    public partial class Form1 : Form
+    {
+        List<string> CardNames;
+        List<int> Amount;
+
+        AutoCompleteStringCollection source;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CardNames = new List<string>();
+            Amount = new List<int>();
+
+            StreamReader streamReader = new StreamReader("cards.csv");
+            string[] totalData = new string[File.ReadAllLines("cards.csv").Length];
+            totalData = streamReader.ReadLine().Split(';');
+            while (!streamReader.EndOfStream)
+            {
+                totalData = streamReader.ReadLine().Split(';');
+                CardNames.Add(totalData[0]);
+                Amount.Add(Int32.Parse(totalData[1]));
+            }
+
+            redraw();
+
+            source = new AutoCompleteStringCollection();
+            source.AddRange(CardNames.ToArray());
+
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            textBox1.AutoCompleteCustomSource = source;
+        }
+
+        private void ownedCardsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            int i = 0;
+
+            
+            while (i < CardNames.Count && CardNames[i].ToLower() != text.ToLower())
+                i++;
+
+            if (i < CardNames.Count)
+            {
+                Amount[i] += Int32.Parse(textBox2.Text);
+                redraw();
+            }
+            textBox1.Text = "";
+            textBox2.Text = "1";
+        }
+
+        public void redraw()
+        {
+            ownedCardsListBox.BeginUpdate();
+            ownedCardsListBox.Items.Clear();
+            for (int i = 0; i < Amount.Count; i++)
+            {
+                if (Amount[i] > 0)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    builder.Append(Amount[i]);
+                    builder.Append("\t");
+                    builder.Append(CardNames[i]);
+                    ownedCardsListBox.Items.Add(builder);
+                }
+            }
+            ownedCardsListBox.EndUpdate();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            int selected = ownedCardsListBox.SelectedIndex;
+            if (selected != -1)
+            {
+                string selectedString = ownedCardsListBox.Items[selected].ToString();
+                string[] cardName = selectedString.Split('\t');
+
+                int i = 0;
+                while (i < CardNames.Count && CardNames[i].ToLower() != cardName[1].ToLower())
+                    i++;
+
+                if (i < CardNames.Count)
+                {
+                    --Amount[i];
+                    redraw();
+                }
+            }
+        }
+
+        private void removeAllButton_Click(object sender, EventArgs e)
+        {
+            int selected = ownedCardsListBox.SelectedIndex;
+            if (selected != -1)
+            {
+                string selectedString = ownedCardsListBox.Items[selected].ToString();
+                string[] cardName = selectedString.Split('\t');
+
+                int i = 0;
+                while (i < CardNames.Count && CardNames[i].ToLower() != cardName[1].ToLower())
+                    i++;
+
+                if (i < CardNames.Count)
+                {
+                    Amount[i] = 0;
+                    redraw();
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder csv = new StringBuilder();
+
+            for (int i = 0; i < Amount.Count; i++)
+            {
+                string cardName = CardNames[i].ToString();
+                string amount = Amount[i].ToString();
+                var newLine = string.Format("{0};{1}", cardName, amount);
+                csv.AppendLine(newLine);
+
+            }
+
+            File.WriteAllText("cards.csv", csv.ToString());
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
